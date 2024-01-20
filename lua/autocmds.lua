c8291f7292f307d5fd9707bd3a1804c1cf46a56a
bk <643439627@qq.com>
2024-01-21 00:44:03 +0800

    update

--- lua/autocmds.lua
+++ lua/autocmds.lua
@@ -4,16 +4,4 @@ vim.api.nvim_create_autocmd({ "BufEnter" }, {
         vim.opt.formatoptions = vim.opt.formatoptions - { "c", "r", "o" }
     end,
 })
--- Normalize paths for Windows buffers
-if vim.fn.has("win32") == 1 then
-  vim.api.nvim_create_autocmd({ "BufAdd" }, {
-    callback = function()
-      local name = vim.api.nvim_buf_get_name(0)
-      if name:sub(2, 2) == ":" then
-        name = name:gsub("\\", "/"):gsub("^%l", string.upper)
-        vim.api.nvim_buf_set_name(0, name)
-      end
-    end,
-  })
-end
 
